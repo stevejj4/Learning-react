b301f6e956f07d42df1a7b338050c6c2f5226f9a
stevejj4 <jumaomondi16@gmail.com>
2025-02-07 23:10:14 +0300

    Testing commit error

---
+++ learnig-react/src/App.js
@@ -0,0 +1,20 @@
+function Profile() {
+  return (
+    <img
+    src="https://i.imgur.com/MK3eW3Am.jpg"
+    alt = "Katherine Johnson"
+    />
+  );
+}
+
+export default function Gallery() {
+  return (
+    <section>
+      <hi>The War Room Members</hi>
+      <Profile />
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
